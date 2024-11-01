a271df006a2e6bd88f8c1abfcc8625c87fab2006
MarcyC-art <marcela.camacho@uao.edu.co>
2024-10-31 19:34:31 -0500

    EntregaActividadesCorregido

--- ProyectoBDDM/ProyectoBDDM/models/entrega_actividades.cs
+++ ProyectoBDDM/ProyectoBDDM/models/entrega_actividades.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace ProyectoBDDM.models
 {
-    internal class entrega_actividades
+    internal class Entrega_actividades
     {
+    
+
+    private int idEA;
+    private string fechaEntrega;
+    private string descripcionEntrega; 
+    private string archivoEntrega;
+    private double calificacionEntrega;
+    private string fechaCalificacion;
+    private string comentarioEntrega;
+    
+    
+
+        public Entrega_actividades(int idEA, string fechaEntrega, string descripcionEntrega, string archivoEntrega, double calificacionEntrega, string fechaCalificacion, string comentarioEntrega)
+        {
+            this.idEA = idEA;
+            this.fechaEntrega = fechaEntrega;
+            this.descripcionEntrega = descripcionEntrega;
+            this.archivoEntrega = archivoEntrega;
+            this.calificacionEntrega = calificacionEntrega;
+            this.fechaCalificacion = fechaCalificacion;
+            this.comentarioEntrega = comentarioEntrega;
+
+        }
     }
 
 
